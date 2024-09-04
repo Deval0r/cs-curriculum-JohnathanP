a85ce26ef0958f9204ca1834d75082b52617de66
Deval0r <jpennock5701@student.pps.net>
2024-09-04 15:27:55 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float xspeed;
+    float xdirection;
+    float xvector;
+
     public bool overworld; 
 
     private void Start()
     {
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
-        
+        xspeed = 1;
+        xdirection = 0;
+        xvector = 0;
         
         if (overworld)
         {
@@ -25,7 +31,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        
+       xdirection = Input.GetAxis("Horizontal");
+       xvector = xspeed * xdirection * Time.deltaTime;
+        transform.Translate(xvector, 0, 0);
+        // had float on xdirection and xvector
     }
     
     //for organization, put other built-in Unity functions here
